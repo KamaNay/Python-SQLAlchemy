3b9edbdcfb7778659c1e3e46c4c977336794c7a8
KamaNay <andrenoronha34@gmail.com>
2026-05-04 14:46:23 -0300

    modified:   db.py 	new file:   requirements.txt

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
 from config import config
 
-engine = create_engine(f"mysql://{config.db_user}:{config.db_password}@{config.db_host}:{config.db_port}/mydb")
+engine = create_engine(
+    f"mysql+pymysql://{config.db_user}:{config.db_password}@{config.db_host}:{config.db_port}/{config.db_name}"
+)
